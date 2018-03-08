b89a3d84b7300c1e3b47e5e14a25312f8fd6512c
jackson-lenhart <jacksonlenhart@gmail.com>
2018-03-07 21:29:32 -0500

    Add insert without AVL fix

--- main.js
+++ main.js
@@ -2,6 +2,26 @@
 
 const { range } = require("./utils");
 
+function Node(value, left = null, right = null) {
+  return {
+    value,
+    left,
+    right
+  };
+}
+
+function insert(value, root, parent = null) {
+  if (value >= root.value) {
+    root.right ?
+      insert(value, root.right, root) :
+      root.right = Node(value);
+  } else {
+    root.left ?
+      insert(value, root.left, root) :
+      root.left = Node(value);
+  }
+}
+
 function buildAvlTree(sortedArr, root) {
   if (sortedArr.length === 0) {
     return;
@@ -20,16 +40,11 @@ function buildAvlTree(sortedArr, root) {
   }
 }
 
-function Node(value, left = null, right = null) {
-  return {
-    value,
-    left,
-    right
-  };
-}
-
 let root = Node(null);
-let nums = range(1, 100);
+let nums = range(1, 8);
 
 buildAvlTree(nums, root);
-console.log(JSON.stringify(root, null, 2));
+console.log("Before insert:", JSON.stringify(root, null, 2));
+
+insert(5.5, root);
+console.log("After insert:", JSON.stringify(root, null, 2));
